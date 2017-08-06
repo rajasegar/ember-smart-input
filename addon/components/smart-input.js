14ed579a068f06e77822b9ad7fcbb54ef68d8e7d
Rajasegar Chandran <rajasegar.c@gmail.com>
2017-08-06 11:55:49 +0530

    Component refactor and package.json update

--- addon/components/smart-input.js
+++ addon/components/smart-input.js
@@ -1,6 +1,17 @@
 import Ember from 'ember';
 import layout from '../templates/components/smart-input';
 
-export default Ember.Component.extend({
-  layout
+const {
+  computed,
+  Component
+} = Ember;
+
+export default Component.extend({
+  layout,
+  tagName: 'span',
+  classNames: ['input'],
+  classNameBindings: ['inputStyle'],
+  inputStyle: computed('style', function() {
+    return `input--${this.get('style')}`;
+  })
 });
